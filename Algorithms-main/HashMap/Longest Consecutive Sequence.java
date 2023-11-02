a3846f1787d4ab1a85db86ba9df3a18df7328acf
sonisg <38506968+sonisg@users.noreply.github.com>
2023-11-02 13:55:38 +0530

    Update Longest Consecutive Sequence.java

--- Algorithms-main/HashMap/Longest Consecutive Sequence.java
+++ Algorithms-main/HashMap/Longest Consecutive Sequence.java
@@ -25,6 +25,34 @@ class Solution {
     }
 }
 
+public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        HashSet<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) { // Check if it's the start of a sequence
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
 
 
 // Example 1:
