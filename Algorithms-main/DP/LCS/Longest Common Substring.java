8c2e0ef6a9218a8da49e3a6ce1d2ecc101c0dc7b
sonisg <38506968+sonisg@users.noreply.github.com>
2023-11-29 22:52:27 +0530

    Create Longest Common Substring.java

---
+++ Algorithms-main/DP/LCS/Longest Common Substring.java
@@ -0,0 +1,24 @@
+class Solution {
+    int longestCommonSubstr(String S1, String S2, int n, int m) {
+        int[][] dp = new int[n + 1][m + 1];
+
+        int maxLength = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (S1.charAt(i - 1) == S2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    maxLength = Math.max(maxLength, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
+// Input: S1 = "ABCDGH", S2 = "ACDGHR", n = 6, m = 6
+// Output: 4
+// Explanation: The longest common substring
+// is "CDGH" which has length 4.
