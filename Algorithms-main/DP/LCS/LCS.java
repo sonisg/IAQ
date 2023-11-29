054d84339cf5e329104ad1901e46c59912020475
sonisg <38506968+sonisg@users.noreply.github.com>
2023-11-29 22:30:57 +0530

    Create LCS.java

---
+++ Algorithms-main/DP/LCS/LCS.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 1; i<=m;i++){
+            for(int j =1;j<=n;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+        
+    }
+}
+
+// Example 1:
+
+// Input: text1 = "abcde", text2 = "ace" 
+// Output: 3  
+// Explanation: The longest common subsequence is "ace" and its length is 3.
+// Example 2:
+
+// Input: text1 = "abc", text2 = "abc"
+// Output: 3
+// Explanation: The longest common subsequence is "abc" and its length is 3.
