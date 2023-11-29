25f4dc62230e19e756cb7dd11f52e29bad8dbd33
sonisg <38506968+sonisg@users.noreply.github.com>
2023-11-29 23:13:07 +0530

    Create longestPalindromeSubseq.java

---
+++ Algorithms-main/DP/LCS/longestPalindromeSubseq.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        String reversed = new StringBuilder(s).reverse().toString();
+        return longestCommonSubsequence(s, reversed);
+    }
+
+    private static int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+}
+
+
+// Example 1:
+
+// Input: s = "bbbab"
+// Output: 4
+// Explanation: One possible longest palindromic subsequence is "bbbb".
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: 2
+// Explanation: One possible longest palindromic subsequence is "bb".
