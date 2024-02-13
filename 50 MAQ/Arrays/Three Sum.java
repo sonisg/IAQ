24602fa9f08fd991d49661839f20a99d336e8895
sonisg <38506968+sonisg@users.noreply.github.com>
2024-02-14 00:51:11 +0530

    Create Three Sum.java

---
+++ 50 MAQ/Arrays/Three Sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+    
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
+        for(int i =0;i<nums.length-2;i++){
+            int j =i+1;
+            int k = nums.length-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    result.add(Arrays.asList(nums[i], nums[j],nums[k]));
+                    j++;
+                    k--;
+                }else if(sum<0){
+                    j++;
+                }else{
+                    k--;
+                }
+            }  
+        }
+        return new ArrayList<>(result);
+    }
+}
