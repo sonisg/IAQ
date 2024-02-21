0e15e116e8ec68189797128e85a9b44b59e4b652
sonisg <38506968+sonisg@users.noreply.github.com>
2024-02-21 11:07:43 +0530

    Create Set Zeros.java

---
+++ 50 MAQ/Arrays/Set Zeros.java
@@ -0,0 +1,58 @@
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+        
+        // Check if the first row has any zeros
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+        
+        // Check if the first column has any zeros
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+        
+        // Mark rows and columns with zeros
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;  // Mark the first element of the row
+                    matrix[0][j] = 0;  // Mark the first element of the column
+                }
+            }
+        }
+        
+        // Set zeros based on the marks
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        // Set zeros for the first row if needed
+        if (firstRowHasZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        // Set zeros for the first column if needed
+        if (firstColHasZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
